47807ede4f4f5df90e6880277670bf43174312f5
[mediba] Kunpei Hikawa <hikawa@mediba.jp>
2017-11-20 17:32:35 +0900

    [ADD] nuxt

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const app = require('express')();
+
 
 exports.httpFunction = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
